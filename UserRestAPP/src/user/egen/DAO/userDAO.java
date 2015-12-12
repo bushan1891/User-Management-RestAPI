7a49674440b42ac266c6b6736e6d44ed9ec67410
bushan1891 <bushan1891@gmail.com>
2015-12-12 11:35:58 -0800

    initial setup

---
+++ UserRestAPP/src/user/egen/DAO/userDAO.java
@@ -0,0 +1,35 @@
+package user.egen.DAO;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import user.egen.model.*;
+
+public class userDAO {
+
+	public List fetchAll() {
+		// TODO Auto-generated method stub
+		List<User> list = new ArrayList<User>();
+		return list;
+	}
+
+	public User fetchOne(int resNo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public User create(User user) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public User update(int cON_ID, User res) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+
+	
+	
+}
